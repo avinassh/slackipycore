636be5aa12bdd81a4ad7cdf0fd4f78036139a9a9
avi <hi@avi.im>
2015-11-07 18:59:07 +0530

    Fix relative imports

--- slackipycore/__init__.py
+++ slackipycore/__init__.py
@@ -1,4 +1,4 @@
-from main import invite, get_users
-from slackipy_exceptions import (AlreadyInTeam, InvalidInviteeEmail,
+from .main import invite, get_users, get_team_info
+from .slackipy_exceptions import (AlreadyInTeam, InvalidInviteeEmail,
                                  InvalidAuthToken, AlreadyInvited,
                                  APIRequestError)
