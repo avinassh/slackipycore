4dfde909d84d9bf86cc1c17d47dce78fc0396ffa
avi <hi@avi.im>
2015-11-11 18:32:45 +0530

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except (IOError, ImportError):
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-version = '0.0.3'
+version = '0.0.4'
 
 setup(
     name='slackipycore',
